aabe66f5a1339ffc06d1eca8b0bd4cd574241f69
Rito Vukela <ritovukela@Admins-MacBook-Pro-3.local>
2017-12-08 17:30:25 +0200

    Add add vehicle activity

---
+++ app/src/main/java/com/vukzrito/bolo/add/AddVehicleActivity.java
@@ -0,0 +1,15 @@
+package com.vukzrito.bolo.add;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vukzrito.bolo.R;
+
+public class AddVehicleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_vehicle);
+    }
+}
---
+++ app/src/main/java/com/vukzrito/bolo/detail/DetailPresenter.java
@@ -0,0 +1,8 @@
+package com.vukzrito.bolo.detail;
+
+public class DetailPresenter implements DetailContract.Interactor {
+    @Override
+    public void loadVehicleDetails(String vehicleId) {
+
+    }
+}
