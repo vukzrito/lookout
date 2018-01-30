42c496563e0f8578ed7d7646e7afcdba1e6f0af5
Rito Vukela <vukzrito@gmail.com>
2018-01-30 23:15:23 +0200

    Integrate Firebase advertisements

---
+++ app/src/main/java/com/vukzrito/bolo/util/Constants.java
@@ -0,0 +1,7 @@
+package com.vukzrito.bolo.util;
+
+
+public class Constants {
+    public static final String ADMOB_APP_ID = "ca-app-pub-3940256099942544/6300978111";// prod "ca-app-pub-4275089733786409~5859331962";
+
+}
