8c0e1c9b7f5d01e1676fd4adf713d8322cb97bb2
Rito Vukela <vukzrito@gmail.com>
2018-01-30 23:45:35 +0200

    Add UI Test

---
+++ app/src/androidTest/java/com/vukzrito/bolo/add/AddVehicleActivityTest.java
@@ -0,0 +1,32 @@
+package com.vukzrito.bolo.add;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.vukzrito.bolo.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+public class AddVehicleActivityTest {
+    @Rule
+    public ActivityTestRule<AddVehicleActivity> activityTestRule = new ActivityTestRule<>(AddVehicleActivity.class);
+
+    @Test
+    public void testThatSubmitButtonSubmitsForm() {
+        onView(withId(R.id.registration_number)).perform(typeText("BK35FXGP"));
+        onView(withId(R.id.vin)).perform(typeText("BTF3PL86F3G5VC4D2"));
+        onView(withId(R.id.make)).perform(typeText("TOYOTA"));
+        onView(withId(R.id.model)).perform(typeText("Corolla"));
+        onView(withId(R.id.color)).perform(typeText("Metallic Blue"), closeSoftKeyboard());
+
+        onView(withId(R.id.btn_submit)).perform(click());
+    }
+
+
+}
---
+++ app/src/androidTest/java/com/vukzrito/bolo/home/VehiclesActivityTest.java
@@ -0,0 +1,24 @@
+package com.vukzrito.bolo.home;
+
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class VehiclesActivityTest {
+
+    @Rule
+    public ActivityTestRule<VehiclesActivity> mActivityTestRule = new ActivityTestRule<>(VehiclesActivity.class);
+
+    @Test
+    public void vehiclesActivityTest() {
+
+
+    }
+
+}
